cb8a5d4676387456bd99fcafc507df6f46158cf1
Eduardo Porciuncula <hello@eduardoluiz.dev>
2025-05-12 12:28:34 -0300

    feat: create sidebar

--- src/app/(public)/_components/footer.tsx
+++ src/app/(public)/_components/footer.tsx
@@ -3,7 +3,10 @@ export function Footer() {
     <footer className="bg-gray-50 py-16">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <p className="text-center text-sm text-gray-500">
-          Todos os direitos reservados &copy; {new Date().getFullYear()}
+          Todos os direitos reservados &copy; {new Date().getFullYear()} -{" "}
+          <span className="hover:text-black duration-300">
+            @_porciunculaedu
+          </span>
         </p>
       </div>
     </footer>
